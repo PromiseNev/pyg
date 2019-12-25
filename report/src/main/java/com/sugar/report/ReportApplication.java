77190714f7d875b941fef0dd94bd8e0b7622a68a
zhangqing <zhangqing@linglie.com>
2019-12-25 14:17:47 +0800

    git init

---
+++ report/src/main/java/com/sugar/report/ReportApplication.java
@@ -0,0 +1,17 @@
+package com.sugar.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 创建时间:   2019/12/25
+ *
+ * @author ZhangQing
+ * 功能描述:
+ */
+@SpringBootApplication
+public class ReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportApplication.class, args);
+    }
+}
